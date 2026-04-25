b21ac5f8ac58d1920d00a7c5dfd1b75518bbfb07
Ravi Ghate <ravighate19@gmail.com>
2026-04-25 22:10:22 +0530

    added new files

---
+++ arrays/GFG_Rotate_array_by_one_place.java
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    public void rotate(int[] arr) {
+        
+        // code here
+        int n=arr.length;
+        if(n<=1) return;
+        int lastElement = arr[n-1];
+        for(int i=n-1; i>=1; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = lastElement;
+    }
+}
